b38191ce47495b79fc4393d0e3a766c9b0bb06f6
Kai <k.davey4@icloud.com>
2026-04-08 21:23:55 -0700

    deployed now?

--- src/main.jsx
+++ src/main.jsx
@@ -5,14 +5,14 @@ import '@atlaskit/css-reset';
 import './index.css';
 import App from './App.jsx';
 
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 setGlobalTheme({ colorMode: 'light', spacing: 'spacing', typography: 'typography-adg3' });
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 );
